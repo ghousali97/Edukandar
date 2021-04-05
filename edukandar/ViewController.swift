43b91182d64beaf42734123ccad63a921edc9157
Ali <ghousali11@gmail.com>
2021-04-06 01:47:48 +0800

    first commit

--- edukandar/ViewController.swift
+++ edukandar/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let swipeLeftRecognizer = UISwipeGestureRecognizer(target: self, action:#selector(handleSwipe(recognizer:)))
+        let swipeRightRecognizer = UISwipeGestureRecognizer(target: self, action:#selector(handleSwipe(recognizer:)))
+        let swipeDownRecognizer = UISwipeGestureRecognizer(target: self, action:#selector(handleSwipe(recognizer:)))
+        
+        
+        swipeLeftRecognizer.direction = .left
+        swipeRightRecognizer.direction = .right
+        swipeDownRecognizer.direction = .down
+        
+        webView.addGestureRecognizer(swipeLeftRecognizer)
+        webView.addGestureRecognizer(swipeRightRecognizer)
+
+        
+        let myURL = URL(string:"https://edukandar.pk/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
     }
+    
+    @objc private func handleSwipe(recognizer: UISwipeGestureRecognizer) {
+        if (recognizer.direction == .left) {
+            if webView.canGoForward {
+                webView.goForward()
+            }
+        }
 
-
+        if (recognizer.direction == .right) {
+            if webView.canGoBack {
+                webView.goBack()
+            }
+        }
+        
+        if (recognizer.direction == .down) {
+            webView.reload()
+        }
+    }
 }
-
