29342f805471370dfec17efef5eadcf74d99e4ca
Ali <ghousali11@gmail.com>
2021-04-05 22:17:48 +0800

    Initial Commit

---
+++ edukandar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  edukandar
+//
+//  Created by Ghous Ali  Khan  on 5/4/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
